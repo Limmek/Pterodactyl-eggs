54c89f12a112f721043c366983f046a552bd9b91
Limmek <jimaugustsson@gmail.com>
2023-11-07 19:43:14 +0100

    vps-ubuntu

--- vps-ubuntu/start.sh
+++ vps-ubuntu/start.sh
@@ -4,12 +4,6 @@ docmd() {
     read -p "container@ubuntu:~ " CMD
     eval "$CMD"
     echo "container@ubuntu:~ "
-    docmd2
-}
-docmd2() {
-    read -p "container@ubuntu:~ " CMD2
-    eval "$CMD2"
-    echo "container@ubuntu:~ "
     docmd
 }
 docmd
