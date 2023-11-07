5fa4fc765adf596fdd61c7d3e5c214797309d9e8
Limmek <jimaugustsson@gmail.com>
2023-11-07 20:41:46 +0100

    vpu-ubuntu

--- vps-ubuntu/entrypoint.sh
+++ vps-ubuntu/entrypoint.sh
@@ -14,11 +14,5 @@ export INTERNAL_IP
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
-if [ ! -f ./start.sh ]; then
-    echo "start.sh not found! Downloading..."
-    curl -sSL -o ./start.sh https://raw.githubusercontent.com/Limmek/Pterodactyl-eggs/main/vps-ubuntu/start.sh
-    chmod +x ./start.sh
-fi
-
 # Run the Server
 eval ${MODIFIED_STARTUP}
